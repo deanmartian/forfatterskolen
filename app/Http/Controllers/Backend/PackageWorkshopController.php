0fa1eb18bb6beaa088f3439a3f01b7863e9e0070
ely butabara <elybutabara@yahoo.com>
2018-10-24 15:36:24 +0800

    fix spacing for email body

--- app/Http/Controllers/Backend/PackageWorkshopController.php
+++ app/Http/Controllers/Backend/PackageWorkshopController.php
@@ -47,7 +47,7 @@ class PackageWorkshopController extends Controller
            $to = $user->email;
            $headers = "From: Forfatterskolen<post@forfatterskolen.no>\r\n";
            //mail($to, $workshop->email_title, $workshop->email_body, $headers);
-           AdminHelpers::send_email($workshop->email_title, 'post@forfatterskolen.no', $to, $workshop->email_body);
+           AdminHelpers::send_email($workshop->email_title, 'post@forfatterskolen.no', $to, nl2br($workshop->email_body));
        }
 
        $workshopTaken->save();
