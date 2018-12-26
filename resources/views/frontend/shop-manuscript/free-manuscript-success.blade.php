3a9c3c5f703e07bb063ba538722ff0aa2f5ca625
ely butabara <elybutabara@yahoo.com>
2018-12-26 10:42:55 +0800

    update free manuscript success

--- resources/views/frontend/shop-manuscript/free-manuscript-success.blade.php
+++ resources/views/frontend/shop-manuscript/free-manuscript-success.blade.php
@@ -8,7 +8,7 @@
 <div class="container text-center">
 	<div class="row">
 		<div class="col-sm-12">
-			<h2 style="margin-top: 120px">Takk for innsendt tekst. Du vil få svar innen fem virkedager.</h2>
+			<h1 style="margin-top: 120px;" class="font-weight-bold">Takk for innsendt tekst. Du vil få svar innen fem virkedager.</h1>
 			<small class="redirect" style="display: inline-block; margin-bottom: 150px"><em>(Du blir sendt til forsiden om
 					<span>5</span> sekunder)</em></small>
 		</div>
@@ -20,13 +20,13 @@
 
 @section('scripts')
 <script>
-	var time = 5;
+	let time = 5;
 	window.setInterval(
 	  function() 
 	  {
 	  	time--;
 	  	console.log(time);
-	  	if(time == 0){
+	  	if(time === 0){
 	  		window.location.href = '{{ url('') }}';
 	  	}
 	  	jQuery('.redirect span').text(time);
