905d1002c0500cac54affbb3f7e052563939f2bf
ely butabara <elybutabara@yahoo.com>
2019-01-14 11:15:59 +0800

    update learner course page

--- resources/views/frontend/learner/course.blade.php
+++ resources/views/frontend/learner/course.blade.php
@@ -4,175 +4,77 @@
 <title>Mine Kurs &rsaquo; Forfatterskolen</title>
 @stop
 
-@section('styles')
-	<style>
-		.table-users .table {
-			margin-top: 12px;
-			margin-bottom: 12px;
-			background-color: #fff;
-			border: solid 1px #ccc;
-		}
-
-		.table thead {
-			background-color: #eee;
-		}
-
-		.nav-tabs {
-			border-bottom: 1px solid #ddd;
-		}
-
-		.nav-tabs>li.active>a, .nav-tabs>li.active>a:hover, .nav-tabs>li.active>a:focus {
-			color: #555;
-			cursor: default;
-			background-color: #fff;
-			border: 1px solid #ddd;
-			border-bottom-color: transparent;
-		}
-
-		.nav-tabs>li>a {
-			background: #f0f2f4;
-			color: #666;
-			outline: medium none;
-			padding: 10px 24px;
-			line-height: 1.42857143;
-			border: 1px solid transparent;
-			border-bottom: 0 none;
-			border-radius: 4px 4px 0 0;
-			position: relative;
-			display: block;
-			margin-right: 8px;
-		}
-	</style>
-@stop
-
 @section('heading') Mine Kurs @stop
 
 @section('content')
-<div class="account-container">
-	
-	@include('frontend.partials.learner-menu')
 
-	<div class="col-sm-12 col-md-10 sub-right-content">
-		<div class="col-sm-12">
-			@include('frontend.partials.learner-search')
-		
-			<div class="row"> 
-			@foreach( Auth::user()->coursesTaken as $courseTaken )
-				<div class="col-sm-12 col-md-4">
-				<div class="dashboard-courses">
-					<div class="course-thumb" style="background-image: url({{$courseTaken->package->course->course_image}})"></div>
-					<div class="course-meta">
-						<strong>{{$courseTaken->package->course->title}}</strong>
-						<p class="no-margin-bottom">
-						{{str_limit(strip_tags($courseTaken->package->course->description), 200)}}
-						</p>
-						@if( $courseTaken->package->course->start_date )
-						<div>Kursstart dato: {{ $courseTaken->package->course->start_date }}</div>
-						@endif
-						{{--original code for slutt dato the other just don't have an else
-						@if( $courseTaken->package->course->end_date )
-						<div>Slutt dato: {{ $courseTaken->package->course->end_date }}</div>
-						@endif--}}
-						@if( $courseTaken->start_date )
-						<div>Start date: {{ $courseTaken->start_date }}</div>
-						@else
-						@endif
-						@if( $courseTaken->end_date )
-							<div>Slutt dato: {{ $courseTaken->end_date }}</div>
-						@else
-							@if( $courseTaken->package->course->end_date )
-								<div>Slutt dato: {{ $courseTaken->package->course->end_date }}</div>
-							@endif
-						@endif
+	<div class="learner-container">
+		<div class="container">
+			<div class="row">
+				@include('frontend.partials.learner-search-new')
+				<div class="row w-100 learner-courses-container adjust-left">
+					@foreach( Auth::user()->coursesTaken as $courseTaken )
+						<div class="col-md-12 col-lg-3 no-right-padding adjust-right-padding">
+							<div class="learner-course card border-0">
+								<div class="course-thumb" style="background-image: url({{$courseTaken->package->course->course_image}})"></div>
+								<div class="course-details card-body">
+									<h3 class="font-weight-normal font-barlow-regular">
+										{{$courseTaken->package->course->title}}
+									</h3>
+									<p class="note-color">
+										{{str_limit(strip_tags($courseTaken->package->course->description), 200)}}
+									</p>
+								</div>
+								<div class="card-footer no-border p-0">
+									@if( $courseTaken->is_active )
+										@if($courseTaken->hasStarted)
+											@if($courseTaken->hasEnded)
+												<button class="btn btn-info" data-toggle="modal" data-target="#renewAllModal">Forny abonnement</button>
+											@else
+												<a class="btn site-btn-global-w-arrow w-100 rounded-0" href="{{route('learner.course.show', ['id' => $courseTaken->id])}}">
+													Fortsett med dette kurset
+												</a>
+												{{-- check if course is webinar-pakke --}}
+                                                <?php
+                                                $package = \App\Package::find($courseTaken->package_id);
+                                                ?>
+												@if ($package && $package->course_id == 17)
 
-						<div class="text-right margin-top">
-							@if( $courseTaken->is_active )
-								@if($courseTaken->hasStarted)
-									@if($courseTaken->hasEnded)
-									<button class="btn btn-info" data-toggle="modal" data-target="#renewAllModal">Forny abonnement</button>
-									@else
-									<a class="btn btn-primary" href="{{route('learner.course.show', ['id' => $courseTaken->id])}}">Fortsett med dette kurset&nbsp;&nbsp;<i class="fa fa-arrow-right"></i></a>
-										{{-- check if course is webinar-pakke --}}
-										<?php
-                                        $package = \App\Package::find($courseTaken->package_id);
-										?>
-										@if ($package && $package->course_id == 17)
+                                                    <?php
 
-											<?php
+                                                    $checkDate = date('m/Y', strtotime($courseTaken->started_at));
 
-												$checkDate = date('m/Y', strtotime($courseTaken->started_at));
+                                                    if ($courseTaken->end_date) {
+                                                        $checkDate = date('m/Y', strtotime($courseTaken->end_date));
+                                                    }
 
-												if ($courseTaken->end_date) {
-                                                    $checkDate = date('m/Y', strtotime($courseTaken->end_date));
-												}
+                                                    $now = new DateTime();
+                                                    $input = DateTime::createFromFormat('m/Y', $checkDate);
+                                                    $diff = $input->diff($now); // Returns DateInterval
 
-												$now = new DateTime();
-												$input = DateTime::createFromFormat('m/Y', $checkDate);
-												$diff = $input->diff($now); // Returns DateInterval
-
-												// m is months
-                                            	$withinSixMonths = $diff->y === 0 && $diff->m <= 6;  // true
-
-											?>
-
-											@if($withinSixMonths)
-											<br>
-											{{--<a href="#renewAllModal" class="btn btn-info margin-top renewAllBtn"
-											   data-toggle="modal"
-											   data-action="{{ route('learner.course.renew-all', $courseTaken->id) }}">Renew All</a>--}}
+                                                    // m is months
+                                                    $withinSixMonths = $diff->y === 0 && $diff->m <= 6;  // true
+                                                    ?>
+												@endif
 											@endif
+										@else
+											<form method="POST" action="{{route('learner.course.take')}}">
+												{{csrf_field()}}
+												<input type="hidden" name="courseTakenId" value="{{$courseTaken->id}}">
+												<button type="submit" class="btn site-btn-global-w-arrow w-100 rounded-0 btn-success">Start dette kurset</button>
+											</form>
 										@endif
+									@else
+										<a class="btn site-btn-global disabled w-100 rounded-0">Kurs på vent</a>
 									@endif
-								@else
-								<form method="POST" action="{{route('learner.course.take')}}">
-									{{csrf_field()}}
-									<input type="hidden" name="courseTakenId" value="{{$courseTaken->id}}">
-									<button type="submit" class="btn btn-success">Start dette kurset&nbsp;&nbsp;<i class="fa fa-arrow-right"></i></button>
-								</form>
-								@endif
-							@else
-							<a class="btn btn-warning disabled">Kurs på vent</a>
-							@endif
-
-							{{--@if ($courseTaken->sent_renew_email)
-								<br>
-								<a href="#renewModal" class="btn btn-success renewCourse" data-toggle="modal" style="margin-top: 10px"
-								   data-fields="{{ json_encode($courseTaken) }}">Renew</a>
-							@endif--}}
+								</div>
+							</div>
 						</div>
-					</div>
+					@endforeach
 				</div>
 			</div>
-			@endforeach
-			</div>
-
-			{{--<div class="table-users table-responsive">
-				<table class="table">
-					<thead>
-						<tr>
-							<th>Survey</th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-						@foreach($surveys as $survey)
-							<tr>
-								<td>{{ $survey->title }}</td>
-								<td>
-									<a href="{{ route('learner.survey', $survey->id) }}">
-										Take Survey
-									</a>
-								</td>
-							</tr>
-						@endforeach
-					</tbody>
-				</table>
-			</div>--}}
-
 		</div>
 	</div>
-	<div class="clearfix"></div>
-</div>
 
 <div id="renewModal" class="modal fade" role="dialog">
 	<div class="modal-dialog">
@@ -231,14 +133,14 @@
 	<script>
 		$(function(){
 		    $(".renewCourse").click(function(){
-                var fields = $(this).data('fields');
-                var modal = $('#renewModal');
+                let fields = $(this).data('fields');
+                let modal = $('#renewModal');
                 $("input[name=course_id]").val(fields.id);
             });
 
 		    $(".renewAllBtn").click(function(){
-                var form = $('#renewAllModal form');
-                var action = $(this).data('action');
+                let form = $('#renewAllModal form');
+                let action = $(this).data('action');
                 form.attr('action', action)
 			});
 		})
---
+++ resources/views/frontend/partials/learner-search-new.blade.php
@@ -0,0 +1,15 @@
+<div class="row w-100 learner-search-container adjust-left-padding">
+    <form role="search" class="w-100" method="get" action="{{ route('learner.account.search') }}">
+        <div class="col-md-4 col-sm-12">
+            <h1 class="font-barlow-regular">@yield('heading')</h1>
+        </div>
+        <div class="col-md-5 col-sm-12 float-right">
+            <div class="input-group">
+                <input type="text" class="form-control" name="search" value="{{ Request::input('search') }}" placeholder="Søk kurs, oppgaver, workshops, webinars.." required>
+                <span class="input-group-btn">
+                    <button class="btn" type="submit"><i class="fa fa-search"></i></button>
+                </span>
+            </div>
+        </div>
+    </form>
+</div>
