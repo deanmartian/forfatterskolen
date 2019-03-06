8a06e39e5f27af096c994efc9c280f1c27a0a54c
ely butabara <elybutabara@yahoo.com>
2019-03-06 14:47:54 +0800

    update for free course

--- resources/views/frontend/learner/dashboard.blade.php
+++ resources/views/frontend/learner/dashboard.blade.php
@@ -126,7 +126,7 @@
                         <div class="card global-card mt-3">
                             <div class="card-header">
                                 <h1>
-                                    Gratis kurs tiljgengelig
+                                    Gratis kurs tilgjengelig
                                 </h1>
                             </div>
                             <div class="card-body">
