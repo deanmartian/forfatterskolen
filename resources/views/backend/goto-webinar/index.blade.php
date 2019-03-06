49361cadcf21ec5753d6c1c9c2f0c64a061e77f9
ely butabara <elybutabara@yahoo.com>
2019-03-06 09:43:50 +0800

    added close button

--- resources/views/backend/goto-webinar/index.blade.php
+++ resources/views/backend/goto-webinar/index.blade.php
@@ -58,6 +58,7 @@
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
                     <h4 class="no-margin">Confirmation Email Template</h4>
                 </div>
                 <div class="modal-body">
