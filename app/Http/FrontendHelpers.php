7833e527fca80c700400e4c1a2fd6d5d345e1d3d
ely butabara <elybutabara@yahoo.com>
2019-06-21 16:31:01 +0800

    fix for generating assignment file

--- app/Http/FrontendHelpers.php
+++ app/Http/FrontendHelpers.php
@@ -754,4 +754,51 @@ class FrontendHelpers
             return false;
         }
 	}
+
+    /**
+     * Separate get content from doc file, the other is used for word count
+     * @param $filename
+     * @return string
+     */
+    public static function getContentFromDocFile($filename)
+    {
+        if(file_exists($filename))
+        {
+            if(($fh = fopen($filename, 'r')) !== false )
+            {
+                $headers = fread($fh, 0xA00);
+
+                // 1 = (ord(n)*1) ; Document has from 0 to 255 characters
+                $n1 = ( ord($headers[0x21C]) - 1 );
+
+                // 1 = ((ord(n)-8)*256) ; Document has from 256 to 63743 characters
+                $n2 = ( ( ord($headers[0x21D]) - 8 ) * 256 );
+
+                // 1 = ((ord(n)*256)*256) ; Document has from 63744 to 16775423 characters
+                $n3 = ( ( ord($headers[0x21E]) * 256 ) * 256 );
+
+                // 1 = (((ord(n)*256)*256)*256) ; Document has from 16775424 to 4294965504 characters
+                $n4 = ( ( ( ord($headers[0x21F]) * 256 ) * 256 ) * 256 );
+
+                // Total length of text in the document
+                $textLength = ($n1 + $n2 + $n3 + $n4);
+
+                if ($textLength > 0) {
+                    $extracted_plaintext = fread($fh, $textLength);
+
+                    // simple print character stream without new lines
+                    //echo $extracted_plaintext;
+
+                    // if you want to see your paragraphs in a new line, do this
+                    $extracted_plaintext = nl2br($extracted_plaintext);
+                    $breaks = array("<br />","<br>","<br/>"); // get break tags
+                    $extracted_plaintext = str_ireplace($breaks, "\r\n", $extracted_plaintext); //replace break tags
+                    return $extracted_plaintext;
+                    // need more spacing after each paragraph use another nl2br
+                }
+
+                return false;
+            }
+        }
+    }
 }
