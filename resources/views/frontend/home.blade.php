81a687afe70586b961f27106955fc7de85ccf909
ely butabara <elybutabara@yahoo.com>
2019-07-10 11:30:45 +0800

    minify front-page css

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -6,7 +6,7 @@
 
 @section('styles')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
-    <link rel="stylesheet" href="{{ asset('css/front-page.css?v='.time()) }}">
+    <link rel="stylesheet" href="{{ asset('css/front-page.min.css?v='.time()) }}">
 @stop
 
 @section('content')
