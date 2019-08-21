bb506f2f88e237e3376630583458af43d9d1026e
ely butabara <elybutabara@yahoo.com>
2019-08-21 14:19:38 +0800

    change editor

--- resources/views/backend/shop-manuscript/index.blade.php
+++ resources/views/backend/shop-manuscript/index.blade.php
@@ -442,7 +442,7 @@
 					<?php endif; ?>
 					<div class="form-group">
 						<label>{{ trans('site.body') }}</label>
-						<textarea name="email_content" cols="30" rows="10" class="form-control test" required
+						<textarea name="email_content" cols="30" rows="10" class="form-control" required
 								  id="freeManuscriptEmailContent"><?php echo e($emailTemplate ? $emailTemplate->email_content : ''); ?></textarea>
 					</div>
 
@@ -509,11 +509,47 @@
 		form.find('.modal-title em').text(title);
 	});
 
-    tinymce.init({
+    /*tinymce.init({
         selector:'#freeManuscriptEmailContent',
         height : "300",
         menubar: false,
         toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help'
-    });
+    });*/
+
+    // tinymce
+    let editor_config = {
+        path_absolute: "{{ URL::to('/') }}",
+        height: '15em',
+        selector: '#freeManuscriptEmailContent',
+        plugins: ['advlist autolink lists link image charmap print preview hr anchor pagebreak',
+            'searchreplace wordcount visualblocks visualchars code fullscreen',
+            'insertdatetime media nonbreaking save table contextmenu directionality',
+            'emoticons template paste textcolor colorpicker textpattern'],
+        toolbar1: 'formatselect fontselect fontsizeselect | bold italic underline strikethrough subscript superscript | forecolor backcolor | link | alignleft aligncenter alignright ' +
+        'alignjustify  | removeformat',
+        toolbar2: 'undo redo | bullist numlist | outdent indent blockquote | link unlink anchor image media code | print fullscreen',
+        relative_urls: false,
+        file_browser_callback : function(field_name, url, type, win) {
+            let x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+            let y = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
+
+            let cmsURL = editor_config.path_absolute + '/laravel-filemanager?field_name=' + field_name;
+            if (type == 'image') {
+                cmsURL = cmsURL + '&type=Images';
+            } else {
+                cmsURL = cmsURL + '&type=Files';
+            }
+
+            tinyMCE.activeEditor.windowManager.open({
+                file : cmsURL,
+                title : 'Filemanager',
+                width : x * 0.8,
+                height : y * 0.8,
+                resizable : 'yes',
+                close_previous : 'no'
+            });
+        }
+    };
+    tinymce.init(editor_config);
 </script>
 @stop
