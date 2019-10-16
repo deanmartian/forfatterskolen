8c5280526435da783211be37dcdb79e9a105cb40
ely butabara <elybutabara@yahoo.com>
2019-10-16 14:17:40 +0800

    update contact ladesk

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -259,5 +259,10 @@
             }
         </script>
         @yield('scripts')
+    <script type="text/javascript">
+        (function(d, src, c) { var t=d.scripts[d.scripts.length - 1],s=d.createElement('script');s.id='la_x2s6df8d';s.async=true;s.src=src;s.onload=s.onreadystatechange=function(){var rs=this.readyState;if(rs&&(rs!='complete')&&(rs!='loaded')){return;}c(this);};t.parentElement.insertBefore(s,t.nextSibling);})(document,
+            'https://forfatterskolen.ladesk.com/scripts/track.js',
+            function(e){ LiveAgent.createButton('bocb2pt7', e); });
+    </script>
     </body>
 </html>
