2827b1b89bc62ab835ecedd10953c9de3f2e4001
ely <elybutabara@yahoo.com>
2021-01-11 10:11:31 +0800

    fix for mail spacing

--- resources/views/emails/mail_to_queue.blade.php
+++ resources/views/emails/mail_to_queue.blade.php
@@ -12,7 +12,7 @@
 <table cellpadding="0" cellspacing="0" border="0" style='font-weight: 300!important;
         font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"'>
     <tr>
-        <td style="padding-top: .3rem">{!! $email_message !!} </td>
+        <td style="padding-top: .3rem">{!! nl2br($email_message) !!} </td>
     </tr>
 </table>
 
