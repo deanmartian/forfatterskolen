50e1c7ddb6130275748050766f3abcf50811c8c7
ely <elybutabara@yahoo.com>
2021-02-18 09:47:04 +0800

    use translation

--- resources/views/frontend/partials/learner-menu-new.blade.php
+++ resources/views/frontend/partials/learner-menu-new.blade.php
@@ -3,55 +3,55 @@
         <li class="nav-item @if(!Request::is('account/course-webinar') && Request::is('account/course*')) active @endif">
             <a class="nav-link" href="{{route('learner.course')}}">
                 <i class="sprite-menu student-cap d-block"></i>
-                Mine Kurs
+                {!! trans('site.learner.nav.course') !!}
             </a>
         </li>
         <li class="nav-item @if(Request::is('account/shop-manuscript*')) active @endif">
             <a class="nav-link" href="{{route('learner.shop-manuscript')}}">
                 <i class="sprite-menu file d-block"></i>
-                Manuskripter
+                {!! trans('site.learner.nav.manuscript') !!}
             </a>
         </li>
         <li class="nav-item @if(Request::is('account/workshop*')) active @endif">
             <a class="nav-link" href="{{route('learner.workshop')}}">
                 <i class="sprite-menu briefcase d-block"></i>
-                Skriveverksted
+                {!! trans('site.learner.nav.workshop') !!}
             </a>
         </li>
         <li class="nav-item @if(Request::is('account/webinar*')) active @endif">
             <a class="nav-link" href="{{route('learner.webinar')}}">
                 <i class="sprite-menu play-button d-block"></i>
-                Felleswebinarer
+                {!! trans('site.learner.nav.webinars') !!}
             </a>
         </li>
         <li class="nav-item @if(Request::is('account/course-webinar*')) active @endif">
             <a class="nav-link" href="{{route('learner.course-webinar')}}">
                 <i class="sprite-menu play-button d-block"></i>
-                Kurswebinarer
+                {!! trans('site.learner.nav.course-webinars') !!}
             </a>
         </li>
         <li class="nav-item @if(Request::is('account/assignment*')) active @endif">
             <a class="nav-link" href="{{route('learner.assignment')}}">
                 <i class="sprite-menu agenda d-block"></i>
-                Oppgaver
+                {!! trans('site.learner.nav.assignment') !!}
             </a>
         </li>
         <li class="nav-item @if(Request::is('account/calendar')) active @endif">
             <a class="nav-link" href="{{route('learner.calendar')}}">
                 <i class="sprite-menu calendar d-block"></i>
-                Kalender
+                {!! trans('site.learner.nav.calendar') !!}
             </a>
         </li>
         <li class="nav-item @if(Request::is('account/invoice*')) active @endif">
             <a class="nav-link" href="{{route('learner.invoice')}}">
                 <i class="sprite-menu list-on-window d-block"></i>
-                Fakturaer
+                {!! trans('site.learner.nav.invoice') !!}
             </a>
         </li>
         <li class="nav-item @if(Request::is('account/upgrade*')) active @endif">
             <a class="nav-link" href="{{route('learner.upgrade')}}">
                 <i class="sprite-menu internet d-block"></i>
-                Kurspakker - Oppgradering
+                {!! trans('site.learner.nav.upgrade') !!}
             </a>
         </li>
         {{--<li class="nav-item @if(Request::is('account/competition')) active @endif">
@@ -63,14 +63,14 @@
         <li class="nav-item @if(Request::is('account/private-message')) active @endif">
             <a class="nav-link" href="{{route('learner.private-message')}}">
                 <i class="fa fa-comment fa-use-comment d-block" style="color: #000; height: 20px; width: 20px"></i>
-                Beskjeder
+                {!! trans('site.learner.nav.message') !!}
             </a>
         </li>
 
         <li class="nav-item @if(Request::is('account/profile')) active @endif">
             <a class="nav-link" href="{{route('learner.profile')}}">
                 <i class="sprite-menu user d-block"></i>
-                Profil \ Kursbevis
+                {!! trans('site.learner.nav.profile') !!}
             </a>
         </li>
     </ul>
