35ced95b2a30b6e0fb003d6d22043a2bc5de2944
ely <elybutabara@yahoo.com>
2021-03-05 15:42:04 +0800

    remove vipps e-faktura button on paid invoices

--- resources/views/backend/learner/show.blade.php
+++ resources/views/backend/learner/show.blade.php
@@ -570,7 +570,7 @@
 										</button>
 									{{--@endif--}}
 
-									@if ($invoice->fiken_invoice_id)
+									@if ($invoice->fiken_invoice_id && !$invoice->fiken_is_paid)
 										<button class="btn btn-success btn-xs vippsFakturaBtn" style="margin-top: 5px"
 												data-toggle="modal"
 											data-target="#vippsFakturaModal"
