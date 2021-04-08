6268667de1058363b124f0c7c4305a1d92eefb81
ely <elybutabara@yahoo.com>
2021-04-08 15:05:16 +0800

    fix for assigned webinar display date

--- resources/views/editor/assigned-webinars.blade.php
+++ resources/views/editor/assigned-webinars.blade.php
@@ -48,8 +48,8 @@
                                 <i class="fa fa-book" aria-hidden="true"></i>&nbsp;{{ trans('site.front.course-text') }}: <span class="course-title">{{ $webinar->webinar->course->title }}</span><br>
                                 <i class="fa fa-calendar" aria-hidden="true"></i>&nbsp; 
                                 {{ str_replace(['_date_', '_time_'],
-                                    [\Carbon\Carbon::parse($webinar->start_date)->format('d.m.Y'),
-                                    \Carbon\Carbon::parse($webinar->start_date)->format('H:i')],
+                                    [\Carbon\Carbon::parse($webinar->webinar->start_date)->format('d.m.Y'),
+                                    \Carbon\Carbon::parse($webinar->webinar->start_date)->format('H:i')],
                                     trans('site.front.our-course.show.start-date')) }} <br>
                                 <i class="fa fa-link" aria-hidden="true"></i>&nbsp;{{ trans('site.presenter-url') }}&nbsp;<a href="{{ $webinar->presenter_url }}">{{ $webinar->presenter_url }}</a>
                             </div>
