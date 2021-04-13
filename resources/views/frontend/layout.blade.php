b683f0886bc362ae88307030b3fa22cd6bda8b2c
ely <elybutabara@yahoo.com>
2021-04-13 15:25:40 +0800

    update facebook pixel

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -72,7 +72,7 @@
         @yield('styles')
 
     <!-- ConnectRetarget PowerPixel -->
-        <script>
+        {{--<script>
             var CRConfig = {
                 'pixel_prefix':'forfatterskolen1',
                 'init_fb':false,
@@ -80,7 +80,22 @@
             };
         </script>
         <script src='//connectio.s3.amazonaws.com/connect-retarget.js?v=1.1'></script>
-        <noscript><img height='1' width='1' style='display:none' src='https://www.facebook.com/tr?id=216415385555961&ev=PageView&noscript=1' /></noscript>
+        <noscript><img height='1' width='1' style='display:none' src='https://www.facebook.com/tr?id=216415385555961&ev=PageView&noscript=1' /></noscript>--}}
+
+        <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '216415385555961');
+            fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+                       src="https://www.facebook.com/tr?id=216415385555961&ev=PageView&noscript=1"/></noscript>
         <!-- End ConnectRetarget PowerPixel -->
 
         <script  async>
