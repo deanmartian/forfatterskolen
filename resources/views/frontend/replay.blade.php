7b90f59e8604ac3881e0e65ab5b47308c734f100
ely <elybutabara@yahoo.com>
2021-04-16 15:07:57 +0800

    minor text update

--- resources/views/frontend/replay.blade.php
+++ resources/views/frontend/replay.blade.php
@@ -53,7 +53,7 @@
 
                 <div class="text-center w-100">
                     <a href="https://forfatterskolen.no/course/66/checkout?c=Bok21" class="btn btn-dark-red">
-                        Melder du på nå får du en rabatt på kroner 3000,- (gjelder alle kurspakkene: basic, standard og pro)
+                        Meld deg på nå og få en rabatt på kroner 3000,- (gjelder alle kurspakkene: Basic, Standard og Pro)
                     </a>
 
                     <a href="{{ url('/storage/files/Minner_og_løgn_april_2021.pdf') }}" class="d-block" download>
