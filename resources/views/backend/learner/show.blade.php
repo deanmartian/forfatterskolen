da9a65f059662829e25ca98990e36548c5f11b1d
ely <elybutabara@yahoo.com>
2022-05-03 10:30:32 +0800

    fix for route error

--- resources/views/backend/learner/show.blade.php
+++ resources/views/backend/learner/show.blade.php
@@ -311,7 +311,7 @@
 													<table class="table table-bordered no-margin-bottom">
 														@foreach( $courseTaken->package->course->lessons as $lesson )
 															<tr>
-																<td><a href="{{ route('admin.lesson.edit', ['course_id' => $courseTaken->package->course->id, 'lesson_id' => $lesson->id]) }}">{{ $lesson->title }}</a></td>
+																<td><a href="{{ route('admin.lesson.edit', ['course_id' => $courseTaken->package->course->id, 'lesson' => $lesson->id]) }}">{{ $lesson->title }}</a></td>
 																<td>
 																	@if( FrontendHelpers::hasLessonAccess($courseTaken, $lesson) )
 																		<button class="btn btn-primary btn-xs defaultAllowAccessBtn" data-toggle="modal" data-target="#lessonDefaultAccessModal" data-action="{{ route('admin.course_taken.default_lesson_access', ['course_taken_id' => $courseTaken->id, 'lesson_id' => $lesson->id]) }}">{{ trans('site.default-access') }}</button>
