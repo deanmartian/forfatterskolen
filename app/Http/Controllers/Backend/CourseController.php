27e2565fd787f71393bab5452641147d3028e7db
ely <elybutabara@yahoo.com>
2023-01-16 11:24:55 +0800

    added description on export hidden webinars

--- app/Http/Controllers/Backend/CourseController.php
+++ app/Http/Controllers/Backend/CourseController.php
@@ -933,11 +933,11 @@ class CourseController extends Controller
     {
         $course = Course::find($course_id);
         if ($course) {
-            $headers = ['title', 'webinar id'];
+            $headers = ['title', 'description', 'webinar id'];
             $webinars = [];
 
             foreach ($course->webinars()->where('status', 0)->get() as $webinar) {
-                $webinars[] = [$webinar->title, $webinar->link];
+                $webinars[] = [$webinar->title, $webinar->description, $webinar->link];
             }
             $excel          = \App::make('excel');
             return $excel->download(new GenericExport($webinars, $headers), 'Hidden Webinars.xlsx');
