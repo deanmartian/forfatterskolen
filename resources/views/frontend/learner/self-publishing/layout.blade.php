e1bcbe6f8af08694e867427e0169a6ee0c8c8c13
elybutabara <elybutabara@yahoo.com>
2024-01-09 15:51:55 +0800

    change fb pixel code

--- resources/views/frontend/learner/self-publishing/layout.blade.php
+++ resources/views/frontend/learner/self-publishing/layout.blade.php
@@ -42,21 +42,23 @@
               integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
         @yield('styles')
 
+        <!-- Meta Pixel Code -->
         <script>
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '216415385555961');
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '3571512856438564');
             fbq('track', 'PageView');
-        </script>
-        <noscript><img height="1" width="1" style="display:none"
-                       src="https://www.facebook.com/tr?id=216415385555961&ev=PageView&noscript=1" alt="facebook image"/></noscript>
-        <!-- End ConnectRetarget PowerPixel -->
+            </script>
+            <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=3571512856438564&ev=PageView&noscript=1"
+            /></noscript>
+        <!-- End Meta Pixel Code -->
 
         <script  async>
             window.Laravel = '{{ json_encode(['csrfToken' => csrf_token()]) }}';
