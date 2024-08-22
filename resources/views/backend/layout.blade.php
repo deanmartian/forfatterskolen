613353e5a95d1f3eb0b3c47d42010ac955c02241
elybutabara <elybutabara@yahoo.com>
2024-08-22 17:06:45 +0800

    updated tinymce api

--- resources/views/backend/layout.blade.php
+++ resources/views/backend/layout.blade.php
@@ -76,7 +76,7 @@
         <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
         @if (!in_array(Route::currentRouteName(),['backend.dashboard', 'admin.course.show', 'admin.learner.show',
              'admin.free-manuscript.index']))
-            <script src="https://cdn.tiny.cloud/1/at9h19nmgr4aahzi0km0mkwkejc37a686yxis2zks33o0p6n/tinymce/5/tinymce.min.js"
+            <script src="https://cdn.tiny.cloud/1/ktrp7pnky7grwhkgft8m2lw12pv3d1aq5i9wanmihifc4l5m/tinymce/5/tinymce.min.js"
                 referrerpolicy="origin"></script>
         @endif
         <script>
