c14b839d5d416c999122011f65eba6e93f2b932d
elybutabara <elybutabara@yahoo.com>
2024-09-09 08:58:37 +0800

    update tinymce

--- resources/views/backend/layout.blade.php
+++ resources/views/backend/layout.blade.php
@@ -76,7 +76,7 @@
         <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
         @if (!in_array(Route::currentRouteName(),['backend.dashboard', 'admin.learner.show',
              'admin.free-manuscript.index']))
-            <script src="https://cdn.tiny.cloud/1/at9h19nmgr4aahzi0km0mkwkejc37a686yxis2zks33o0p6n/tinymce/5/tinymce.min.js"
+            <script src="https://cdn.tiny.cloud/1/ktrp7pnky7grwhkgft8m2lw12pv3d1aq5i9wanmihifc4l5m/tinymce/5/tinymce.min.js"
                 referrerpolicy="origin"></script>
         @endif
         <script>
@@ -145,7 +145,7 @@
                     button.addEventListener('click', function() {
                         if (typeof tinymce === 'undefined') {
                             var script = document.createElement('script');
-                            script.src = "https://cdn.tiny.cloud/1/at9h19nmgr4aahzi0km0mkwkejc37a686yxis2zks33o0p6n	/tinymce/5/tinymce.min.js";
+                            script.src = "https://cdn.tiny.cloud/1/ktrp7pnky7grwhkgft8m2lw12pv3d1aq5i9wanmihifc4l5m/tinymce/5/tinymce.min.js";
                             script.referrerPolicy = "origin";
                             script.onload = initTinyMCE;
                             document.body.appendChild(script);
