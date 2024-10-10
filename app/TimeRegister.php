83d56f7ab76cdff70bb7edc52cee27c52ee4f3b2
elybutabara <elybutabara@yahoo.com>
2024-10-10 10:07:47 +0800

    allow empty file in time register

--- app/TimeRegister.php
+++ app/TimeRegister.php
@@ -36,14 +36,15 @@ class TimeRegister extends Model
     public function getFileLinkAttribute()
     {
         $fileLink = '';
-        $filename = $this->attributes['invoice_file'];
-
-        $extension = explode('.', basename($filename));
-        if( end($extension) == 'pdf' || end($extension) == 'odt' ) {
-            $fileLink = '<a href="/js/ViewerJS/#../..'.$filename.'">'.basename($filename).'</a>';
-        } elseif( end($extension) == 'docx' || end($extension) == 'doc' ) {
-            $fileLink = '<a href="https://view.officeapps.live.com/op/embed.aspx?src='.url('').$filename.'">'
-                .basename($filename).'</a>';
+        if (array_key_exists('invoice_file', $this->attributes)) {
+            $filename = $this->attributes['invoice_file'];
+            $extension = explode('.', basename($filename));
+            if( end($extension) == 'pdf' || end($extension) == 'odt' ) {
+                $fileLink = '<a href="/js/ViewerJS/#../..'.$filename.'">'.basename($filename).'</a>';
+            } elseif( end($extension) == 'docx' || end($extension) == 'doc' ) {
+                $fileLink = '<a href="https://view.officeapps.live.com/op/embed.aspx?src='.url('').$filename.'">'
+                    .basename($filename).'</a>';
+            }
         }
 
         return $fileLink;
