f63e1984902c624eafdc6aee960292713544ebac
elybutabara <elybutabara@yahoo.com>
2025-02-25 16:40:05 +0800

    fix for storage book details link

--- app/Http/Controllers/Backend/StorageBookController.php
+++ app/Http/Controllers/Backend/StorageBookController.php
@@ -11,6 +11,7 @@ class StorageBookController extends Controller {
     {
         $projectCentralDistributions = ProjectRegistration::join('project_books', 
             'project_registrations.project_id', '=', 'project_books.project_id')
+            ->select('project_registrations.*', 'book_name')
         ->where([
             'field' => 'central-distribution',
             'project_registrations.in_storage' => 1
