a88e59cc8fa80e19c44c83c7d0307b1f0d4f05df
elybutabara <elybutabara@yahoo.com>
2025-03-05 10:50:13 +0800

    update for self publishing dashboard

--- resources/views/frontend/learner/self-publishing/dashboard.blade.php
+++ resources/views/frontend/learner/self-publishing/dashboard.blade.php
@@ -27,63 +27,71 @@
                             {{ trans('site.author-portal.add-book-project') }}
                         </button>
                     @endif
-                </div>
-                
-                <div class="col-md-12 dashboard-course no-left-padding">
+
                     <div class="card global-card">
                         <div class="card-header">
-                            <h1>
-                                Redaktør {{-- Self Publishing --}}
+                            <h1 class="d-inline-block">
+                                {{ trans('site.author-portal.book-project') }}
                             </h1>
                         </div>
-                        <div class="card-body" style="padding: 0">
-                            <table class="table" style="margin-bottom: 0">
+                        <div class="card-body">
+                            <table class="table">
                                 <thead>
                                 <tr>
-                                    <th>{{ trans('site.title') }}</th>
+                                    <th>{{ trans('site.author-portal.project-number') }}</th>
+                                    <th>{{ trans('site.author-portal.project-name') }}</th>
                                     <th>{{ trans('site.description') }}</th>
-                                    <th>{{ trans_choice('site.files', 1) }}</th>
-                                    <th>{{ trans('site.expected-finish') }}</th>
-                                    <th>{{ trans_choice('site.feedbacks', 1) }}</th>
+                                    <th>{{ trans('site.status') }}</th>
+                                    <th>{{ trans('site.author-portal.standard-project') }}</th>
                                 </tr>
                                 </thead>
                                 <tbody>
-                                @foreach($selfPublishingList as $publishing)
-                                    <tr>
-                                        <td>
-                                            {{ $publishing->title }}
-                                        </td>
-                                        <td>
-                                            {{ $publishing->description }}
-                                        </td>
-                                        <td>
-                                            {!! $publishing->file_link !!}
-                                        </td>
-                                        <td>
-                                            {{ $publishing->expected_finish }}
-                                        </td>
-                                        <td>
-                                            @if($publishing->feedback)
-                                                @if($publishing->feedback->is_approved)
-                                                    {{-- <button class="btn btn-primary btn-xs viewFeedbackBtn"
-                                                            data-target="#viewFeedbackModal"
-                                                            data-toggle="modal"
-                                                            data-fields="{{ json_encode($publishing) }}">
-                                                        View Feedback
-                                                    </button> --}}
-                                                    <a class="btn btn-primary btn-xs viewFeedbackBtn" 
-                                                    href="{{ route('learner.self-publishing.feedback.download', $publishing->feedback->id) }}">
-                                                        {{ trans('site.learner.download-feedback') }}
-                                                    </a>
-                                                @else
-                                                    <label class="label label-warning" style="margin-right: 5px;">
-                                                        {{ trans('site.pending') }}
-                                                    </label>
+                                    @foreach($projects as $project)
+                                        <tr>
+                                            <td>
+                                                {{ $project->identifier }}
+                                            </td>
+                                            <td>
+                                                <a href="{{ route('learner.project.show', $project->id) }}">
+                                                    {{ $project->name }}
+                                                </a>
+                                            </td>
+                                            <td>
+                                                {{ $project->description }}
+                                            </td>
+                                            <td>
+                                                {{ $project->start_date}}
+                                                @if($project->end_date)
+                                                    - {{ $project->end_date }}
                                                 @endif
-                                            @endif
-                                        </td>
-                                    </tr>
-                                @endforeach
+
+                                                <br>
+
+                                                @if($project->status === 'active')
+                                                    <span class="badge badge-primary">
+                                                        {{ trans('site.author-portal.active') }}
+                                                    </span>
+                                                @elseif ($project->status === 'lead')
+                                                    <span class="badge badge-warning">Lead</span>
+                                                @elseif($project->status === 'finished')
+                                                    <span class="badge badge-success">Finished</span>
+                                                @endif
+                                            </td>
+                                            <td>
+                                                @if ($project->is_standard)
+                                                    <span class="badge badge-primary">
+                                                        {{ trans('site.author-portal.current') }}
+                                                    </span>
+                                                @else
+                                                    <button class="btn btn-primary btn-xs standardProjectBtn" data-toggle="modal" 
+                                                    data-action="{{ route('learner.project.set-standard', $project->id) }}"
+                                                    data-target="#standardProjectModal">
+                                                        {{ trans('site.author-portal.set-standard') }}
+                                                    </button>
+                                                @endif
+                                            </td>
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
@@ -91,27 +99,6 @@
                 </div>
             </div>
         </div> <!-- end container -->
-    </div>
-
-    <div id="viewFeedbackModal" class="modal fade" role="dialog" tabindex="-1">
-        <div class="modal-dialog modal-md">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
-                <div class="modal-body">
-                    <div class="form-group">
-                        <label>{{ trans_choice('site.manuscripts', 1) }}</label>
-                        <div id="manus-container"></div>
-                    </div>
-
-                    <div class="form-group">
-                        <label>{{ trans_choice('site.notes', 1) }}</label>
-                        <div id="notes-container" style="white-space: pre;max-height: 500px;overflow: auto;"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 
     <div id="projectModal" class="modal fade" role="dialog">
@@ -147,15 +134,41 @@
         </div>
     </div>
 
+    <div id="standardProjectModal" class="modal fade" role="dialog">
+        <div class="modal-dialog modal-md">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">
+                        {{ trans('site.author-portal.standard-project') }}
+                    </h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <form method="POST" action="" onsubmit="disableSubmit(this)">
+                        {{ csrf_field() }}
+
+                        <p>
+                            Are you sure you want to set this project as <em>standard</em>?
+                        </p>
+
+                        <div class="text-right">
+                            <button class="btn btn-primary" type="submit">{{ trans('site.save') }}</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @stop
 
 @section('scripts')
-    <script>
-        $(".viewFeedbackBtn").click(function(){
-            let modal = $("#viewFeedbackModal");
-            let fields = $(this).data('fields');
-            modal.find("#manus-container").html(fields.feedback.file_link);
-            modal.find("#notes-container").text(fields.feedback.notes);
-        });
-    </script>
+<script>
+    $(".standardProjectBtn").click(function() {
+        const action = $(this).data('action');
+
+        $("#standardProjectModal").find("form").attr("action", action)
+    });
+</script>
 @stop
+
