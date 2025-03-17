026be45a484e8409402f072badbf0c2e4d54a51b
elybutabara <elybutabara@yahoo.com>
2025-03-17 10:14:46 +0800

    added project registration id field

---
+++ database/migrations/2025_03_17_020747_add_registration_id_to_project_book_sales_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRegistrationIdToProjectBookSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('project_book_sales', function (Blueprint $table) {
+            $table->integer('project_registration_id')->nullable()->after('project_book_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('project_book_sales', function (Blueprint $table) {
+            $table->dropColumn('project_registration_id');
+        });
+    }
+}
