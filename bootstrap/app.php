c67d144442618c22185af185128279d732dae49d
Shift <shift@laravelshift.com>
2025-06-13 08:32:27 +0000

    Re-register service providers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,16 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders()
+    ->withProviders([
+        \Laravel\Tinker\TinkerServiceProvider::class,
+        \Laravel\Socialite\SocialiteServiceProvider::class,
+        \Maatwebsite\Excel\ExcelServiceProvider::class,
+        \UniSharp\LaravelFilemanager\LaravelFilemanagerServiceProvider::class,
+        \Intervention\Image\ImageServiceProvider::class,
+        \Barryvdh\TranslationManager\ManagerServiceProvider::class,
+        \Barryvdh\DomPDF\ServiceProvider::class,
+        \Anhskohbo\NoCaptcha\NoCaptchaServiceProvider::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         // api: __DIR__.'/../routes/api.php',
--- config/app.php
+++ config/app.php
@@ -15,36 +15,6 @@ return [
 
     'log_max_files' => env('APP_LOG_MAX_FILES', 2),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-        Laravel\Tinker\TinkerServiceProvider::class,
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-
-        // Other service providers...
-        Laravel\Socialite\SocialiteServiceProvider::class,
-
-        Maatwebsite\Excel\ExcelServiceProvider::class,
-
-        // Laravel File manager providers
-        UniSharp\LaravelFilemanager\LaravelFilemanagerServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
-
-        Barryvdh\TranslationManager\ManagerServiceProvider::class,
-        Barryvdh\DomPDF\ServiceProvider::class,
-
-        Anhskohbo\NoCaptcha\NoCaptchaServiceProvider::class,
-        App\Providers\DropboxServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'AdminHelpers' => App\Http\AdminHelpers::class,
