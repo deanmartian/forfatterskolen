90e0ec44b6c93f18d013fd5f2c708c4558a5314b
Shift <shift@laravelshift.com>
2025-06-13 08:32:26 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,42 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+
+        $middleware->validateCsrfTokens(except: [
+            //
+            '/paypalipn',
+            '/paypalipn*',
+            'paypalipn',
+            'paypalipn*',
+            'paypalipn/*',
+            '/webhook/paypal/*',
+            'webhook/paypal/*',
+            'gotowebinar',
+            'gotowebinar*',
+            'gotowebinar/*',
+            '/vipps/*',
+            '/vipps*',
+            '/vipps/payment/v2/payments/*',
+            '/vipps/payment/v2/payments*',
+            '/fb-leads',
+        ]);
+
+        $middleware->throttleApi();
+
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\Admin::class,
+            'checkAutoRenewCourses' => \App\Http\Middleware\CheckAutoRenewCourses::class,
+            'checkPageAccess' => \App\Http\Middleware\CheckPageAccess::class,
+            'cors' => \App\Http\Middleware\Cors::class,
+            'editor' => \App\Http\Middleware\Editor::class,
+            'giutbok' => \App\Http\Middleware\Giutbok::class,
+            'guest' => \App\Http\Middleware\Guest::class,
+            'headEditor' => \App\Http\Middleware\HeadEditor::class,
+            'learner' => \App\Http\Middleware\Learner::class,
+            'logActivity' => \App\Http\Middleware\LogsActivity::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
