d56ecbde159e603a39287cb9beaa54bf59a934ea
elybutabara <elybutabara@yahoo.com>
2025-06-20 15:02:01 +0800

    disable the $with on shopManuscriptsTaken

--- app/ShopManuscriptsTaken.php
+++ app/ShopManuscriptsTaken.php
@@ -18,8 +18,8 @@ class ShopManuscriptsTaken extends Model
         'expected_finish', 'manuscript_uploaded_date', 'genre', 'description', 'is_manuscript_locked', 'synopsis',
         'coaching_time_later', 'is_welcome_email_sent', 'gift_purchase_id'];
 
-    protected $with = ['shop_manuscript', 'user', 'receivedWelcomeEmail', 'receivedExpectedFinishEmail',
-        'receivedAdminFeedbackEmail', 'receivedFollowUpEmail'];
+    /* protected $with = ['shop_manuscript', 'user', 'receivedWelcomeEmail', 'receivedExpectedFinishEmail',
+        'receivedAdminFeedbackEmail', 'receivedFollowUpEmail']; */
 
     public function user(): BelongsTo
     {
