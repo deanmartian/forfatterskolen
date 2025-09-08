f588a315ac9e08710a4100a4a46bcb8c6dd999c3
elybutabara <elybutabara@yahoo.com>
2025-09-08 11:42:19 +0800

    update for confirm action modal

--- resources/views/editor/coaching-time/index.blade.php
+++ resources/views/editor/coaching-time/index.blade.php
@@ -183,7 +183,7 @@
         </div>
 
         <div class="modal fade" id="actionConfirmModal" tabindex="-1" role="dialog" aria-hidden="true">
-            <div class="modal-dialog" role="document">
+            <div class="modal-dialog modal-sm" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
@@ -193,8 +193,8 @@
                         <p id="actionConfirmMessage">Are you sure?</p>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                        <button type="button" class="btn btn-primary" id="actionConfirmBtn">Confirm</button>
+                        <button type="button" class="btn btn-primary" id="actionConfirmBtn">Yes</button>
+                        <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
                     </div>
                 </div>
             </div>
