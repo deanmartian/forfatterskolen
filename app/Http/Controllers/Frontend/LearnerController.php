9db411571ef4539423633fff17f6267100fd6697
elybutabara <elybutabara@yahoo.com>
2025-11-04 13:44:07 +0800

    added sending of email after deleting assignment manuscript in learner portal

--- app/Http/Controllers/Frontend/LearnerController.php
+++ app/Http/Controllers/Frontend/LearnerController.php
@@ -4947,6 +4947,8 @@ class LearnerController extends Controller
     public function deleteAssignmentManuscript($id): RedirectResponse
     {
         $manuscript = AssignmentManuscript::findOrFail($id);
+        $user = $manuscript->user;
+        $user_email = $user->email;
 
         // delete the file from the server
         $oldManuscript = $manuscript->filename;
@@ -4955,6 +4957,21 @@ class LearnerController extends Controller
         }
 
         $manuscript->forceDelete();
+
+        $emailTemplate = AdminHelpers::emailTemplate('Assignment Manuscript Removed');
+        $emailContent = str_replace([
+            ':firstname',
+            ':assignment',
+            ':button',
+            ':end_button',
+        ], [
+            $manuscript->user->first_name,
+            '<em>'.$manuscript->assignment->title.'</em>'
+
+        ], $emailTemplate->email_content);
+
+        dispatch(new AddMailToQueueJob($user_email, $emailTemplate->subject, $emailContent,
+            $emailTemplate->from_email, null, null, 'learner', $user->id));
 
         return redirect()->back();
     }
