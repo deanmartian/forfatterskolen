087ad38ffae7a1fa6bcd657ee9d55da27ce0a961
elybutabara <35125338+elybutabara@users.noreply.github.com>
2025-12-02 16:25:28 +0800

    Use FullCalendar 5 for learner calendar rendering

--- resources/views/frontend/learner/calendar.blade.php
+++ resources/views/frontend/learner/calendar.blade.php
@@ -7,7 +7,7 @@
 
 @section('styles')
 <link rel="stylesheet" href="{{asset('bootstrap-calendar/css/calendar.min.css')}}">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@3.10.5/dist/fullcalendar.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.css">
 <style>
     .fc-popover.fc-more-popover .fc-header .fc-title {
         color: #000;
@@ -30,12 +30,12 @@
                         <div class="row">
                                 <div class="card w-100 rounded-0 py-4">
                                         <ul class="calendar-guide">
-						<li class="guide-blue">{{ trans('site.learner.script') }}</li>
-						<li class="guide-green">{{ trans('site.learner.assignment') }}</li>
-						<li class="guide-purple">{{ trans('site.learner.webinars') }}</li>
-						<li class="guide-pink">{{ trans('site.learner.modules') }}</li>
-						<li class="guide-orange">{{ trans('site.learner.webinars') }}</li> <!-- course-webinars -->
-						<li class="guide-inverse">{{ trans('site.learner.notes-text') }}</li>
+                                                <li class="guide-blue">{{ trans('site.learner.script') }}</li>
+                                                <li class="guide-green">{{ trans('site.learner.assignment') }}</li>
+                                                <li class="guide-purple">{{ trans('site.learner.webinars') }}</li>
+                                                <li class="guide-pink">{{ trans('site.learner.modules') }}</li>
+                                                <li class="guide-orange">{{ trans('site.learner.webinars') }}</li> <!-- course-webinars -->
+                                                <li class="guide-inverse">{{ trans('site.learner.notes-text') }}</li>
                                                 <li class="guide-red">{{ trans('site.learner.coaching-time') }}</li>
                                         </ul>
                                         <div class="px-3" style="text-align: right;">
@@ -48,9 +48,9 @@
 
                         <div class="row">
                                 <div id="full-calendar"></div>
-			</div>
-		</div>
-	</div>
+                        </div>
+                </div>
+        </div>
 @stop
 
 
@@ -58,124 +58,74 @@
 <script type="text/javascript" src="{{asset('js/underscore-min.js')}}"></script>
 <script type="text/javascript" src="{{asset('bootstrap-calendar/js/language/no-NO.js')}}"></script>
 <script type="text/javascript" src="{{asset('bootstrap-calendar/js/calendar.js')}}"></script>
-<script type="text/javascript" src="https://momentjs.com/downloads/moment-with-locales.js"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/fullcalendar@3.10.5/dist/fullcalendar.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
 <script type="text/javascript">
 
     let translations = {
         today : "{{ trans('site.learner.today-text') }}",
-		month : "{{ trans('site.learner.month-text') }}",
-		week : "{{ trans('site.learner.week-text') }}",
+                month : "{{ trans('site.learner.month-text') }}",
+                week : "{{ trans('site.learner.week-text') }}",
         day : "{{ trans('site.learner.day-text') }}",
         prev : "{{ trans('site.learner.prev-text') }}",
         next : "{{ trans('site.learner.next-text') }}",
-	};
+        };
 
-$('#full-calendar').fullCalendar({
-                locale: 'nb',
-        timezone: 'local',
-        header: { right: 'prev,today,next, month,agendaWeek,agendaDay',
-                        /*center: 'title'*/}, // display the month title
-        displayEventTime: true,
-        views: {
-            month: {
-                displayEventTime: false,
-            },
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('full-calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        locale: 'nb',
+        timeZone: 'local',
+        initialView: 'timeGridWeek',
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
         },
-        eventLimit: true,
-        eventLimitText: '{{ trans('site.view-more') }}',
-        eventLimitClick: 'popover',
-                buttonText: {
-            today:	translations.today,
-            month:	translations.month,
-            week:	translations.week,
-            day:	translations.day,
-			prev:	translations.prev,
-			next:	translations.next
+        buttonText: {
+            today: translations.today,
+            month: translations.month,
+            week: translations.week,
+            day: translations.day,
+            prev: translations.prev,
+            next: translations.next
         },
-		titleFormat: 'MMMM YYYY', // format the month title
-        columnFormat: 'dddd',
-		eventRender: function(eventObj, $el) {
-            $el.popover({
-                title: eventObj.title,
-                content: /*eventObj.description*/'',
-                trigger: 'hover',
-                placement: 'top',
-                container: 'body'
-            });
-        },
-        eventDataTransform: function(event) {
-            const explicitAllDay = event.allDay ?? event.all_day;
-            const startRaw = event.start;
-            const endRaw = event.end;
-
-            const start = moment.parseZone(startRaw);
-            let end = moment.parseZone(endRaw);
-
-            if (!start.isValid()) {
-                return null;
-            }
-
-            const hasExplicitAllDay = explicitAllDay !== undefined && explicitAllDay !== null;
-            const isAllDay = hasExplicitAllDay
-                ? Boolean(explicitAllDay)
-                : start.isValid() && !start.hasTime();
-
-            if (isAllDay) {
-                const allDayStart = start.clone().startOf('day');
-                const allDayEnd = end.isValid() ? end.clone().startOf('day') : allDayStart.clone();
-
-                return {
-                    ...event,
-                    allDay: true,
-                    start: allDayStart.format('YYYY-MM-DD'),
-                    end: allDayEnd.add(1, 'day').format('YYYY-MM-DD'),
-                };
-            }
-
-            if (!end.isValid()) {
-                end = start.clone().add(1, 'hour');
-            }
-
-            if (!end.isAfter(start)) {
-                end = start.clone().add(1, 'hour');
-            }
-
-            return {
-                ...event,
-                allDay: false,
-                start: start.format('YYYY-MM-DD[T]HH:mm:ssZ'),
-                end: end.format('YYYY-MM-DD[T]HH:mm:ssZ'),
-            };
+        eventTimeFormat: {
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false
         },
         events: @json($events)
+    });
+
+    calendar.render();
+});
+
+        let calendar = $("#calendar").calendar(
+                {
+                        language: 'no-NO',
+                        tmpl_path: "{{asset('bootstrap-calendar/tmpls')}}/",
+                        events_source: [
+                                @foreach($events as $event)
+                                {!! json_encode($event) !!},
+                                @endforeach
+                        ],
+                }
+        );
+
+        $('.btn-group button[data-calendar-nav]').each(function() {
+        let $this = $(this);
+                $this.click(function() {
+                        calendar.navigate($this.data('calendar-nav'));
+                });
         });
 
-	let calendar = $("#calendar").calendar(
-		{
-			language: 'no-NO',
-			tmpl_path: "{{asset('bootstrap-calendar/tmpls')}}/",
-			events_source: [
-				@foreach($events as $event)
-				{!! json_encode($event) !!},
-				@endforeach
-			],
-		}
-	);			
-
-	$('.btn-group button[data-calendar-nav]').each(function() {
+        $('.btn-group button[data-calendar-view]').each(function() {
         let $this = $(this);
-		$this.click(function() {
-			calendar.navigate($this.data('calendar-nav'));
-		});
-	});
-
-	$('.btn-group button[data-calendar-view]').each(function() {
-        let $this = $(this);
-		$this.click(function() {
-			calendar.view($this.data('calendar-view'));
-		});
-	});
+                $this.click(function() {
+                        calendar.view($this.data('calendar-view'));
+                });
+        });
 
 </script>
 @stop
