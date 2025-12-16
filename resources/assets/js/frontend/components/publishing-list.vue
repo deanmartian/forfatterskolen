8fa53b27ba6220e9f7cf92a47f4140b7b6f04c2f
elybutabara <elybutabara@yahoo.com>
2025-12-16 15:30:30 +0800

    use translation in publishing page

--- resources/assets/js/frontend/components/publishing-list.vue
+++ resources/assets/js/frontend/components/publishing-list.vue
@@ -2,7 +2,7 @@
     <div class="row publishing-list">
         <div class="col-md-3">
             <h3 class="text-center">
-                Liste over forfattere
+                {{ trans('site.list-of-authors') }}
             </h3>
 
             <div class="authors-list-container">
@@ -30,7 +30,7 @@
 
         <div class="col-md-3 text-center author-book-list-container">
             <h3>
-                Bøker fra forfatter
+                {{ trans('site.books-from-author') }}
             </h3>
             <div class="author-book-list">
                 <template v-for="library in currentBook.libraries">
