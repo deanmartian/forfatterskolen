5e387893805661a9a358f8fe6e3a0080bcadd445
elybutabara <35125338+elybutabara@users.noreply.github.com>
2026-01-29 02:29:55 +0100

    Add lesson availability date to API response

--- app/Http/Resources/Api/V1/LessonResource.php
+++ app/Http/Resources/Api/V1/LessonResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Api\V1;
 
+use App\Http\FrontendHelpers;
 use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -9,6 +10,8 @@ class LessonResource extends JsonResource
 {
     public function toArray($request): array
     {
+        $startedAt = $request->attributes->get('course_started_at');
+
         return [
             'id' => $this->id,
             'course_id' => $this->course_id,
@@ -21,6 +24,7 @@ class LessonResource extends JsonResource
             'period' => $this->period,
             'order' => $this->order,
             'allow_lesson_download' => (bool) $this->allow_lesson_download,
+            'available_at' => FrontendHelpers::lessonAvailability($startedAt, $this->delay, $this->period),
             'created_at' => $this->created_at ? Carbon::parse($this->created_at)->toIso8601String() : null,
             'updated_at' => $this->updated_at ? Carbon::parse($this->updated_at)->toIso8601String() : null,
         ];
