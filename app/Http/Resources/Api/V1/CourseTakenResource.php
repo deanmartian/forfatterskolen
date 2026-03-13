cc93cda63188f541157a4f8abfa4fb8228fa3a47
elybutabara <35125338+elybutabara@users.noreply.github.com>
2026-03-13 07:33:09 +0100

    Use is_upgradeable for upgrade option eligibility

--- app/Http/Resources/Api/V1/CourseTakenResource.php
+++ app/Http/Resources/Api/V1/CourseTakenResource.php
@@ -86,7 +86,7 @@ class CourseTakenResource extends JsonResource
                         : ! ((int) $package->disable_upgrade_price === 1);
                 }
 
-                if (! $displayBtn || ! (bool) $currentPackage->is_show) {
+                if (! $displayBtn || ! (bool) $currentPackage->is_upgradeable) {
                     return null;
                 }
 
